871e70ca948391b8d1ac4971d91cdecc78fced66
gear2-joonyeong <57510872+gus6615@users.noreply.github.com>
2025-10-04 19:34:47 +0900

    TextMeshProUGUIColorHandler 모듈 추가

--- Assets/_Project/StateVisualController/Scripts/Core/Handlers/TextMeshProUGUIHandlers.cs
+++ Assets/_Project/StateVisualController/Scripts/Core/Handlers/TextMeshProUGUIHandlers.cs
@@ -24,6 +24,30 @@ namespace StateVisualController
         }
     }
 
+    /// <summary>
+    /// TextMeshProUGUI 색상 변경을 위한 데이터 클래스
+    /// </summary>
+    [CreateAssetMenu(fileName = "TextMeshProUGUIColorData", menuName = "StateVisualController/TextMeshProUGUI Color Data")]
+    public class TextMeshProUGUIColorData : ScriptableObject
+    {
+        [SerializeField] private Color color = Color.white;
+        
+        public Color Color 
+        { 
+            get => color; 
+            set => color = value; 
+        }
+        
+        private void OnValidate()
+        {
+            // Color 값이 유효한 범위 내에 있는지 확인
+            color.r = Mathf.Clamp01(color.r);
+            color.g = Mathf.Clamp01(color.g);
+            color.b = Mathf.Clamp01(color.b);
+            color.a = Mathf.Clamp01(color.a);
+        }
+    }
+
     // ==================== TMPText 관련 핸들러들 ====================
     
     /// <summary>
@@ -52,6 +76,40 @@ namespace StateVisualController
             var textData = stateData.Data as TextMeshProUGUIContentData;
             EditorGUI.BeginChangeCheck();
             textData.Content = EditorGUILayout.TextField("Text", textData.Content);
+            if (EditorGUI.EndChangeCheck())
+            {
+                EditorUtility.SetDirty(controller);
+            }
+        }
+#endif
+    }
+
+    /// <summary>
+    /// TextMeshProUGUI 컴포넌트의 색상을 변경하는 핸들러
+    /// </summary>
+    public class TextMeshProUGUIColorHandler : BaseStateHandler
+    {
+        public override void ApplyState(StateHandlerData data)
+        {
+            if (targetComponent is TextMeshProUGUI text && data.Data is TextMeshProUGUIColorData colorData)
+            {
+                text.color = colorData.Color;
+            }
+        }
+
+        public override Type[] GetTargetComponentType() => new Type[] { typeof(TextMeshProUGUI) };
+
+#if UNITY_EDITOR
+        public override void DrawFields(StateHandlerData stateData, StateVisualController controller)
+        {
+            if (stateData.Data == null)
+            {
+                stateData.Data = ScriptableObject.CreateInstance<TextMeshProUGUIColorData>();
+            }
+            
+            var colorData = stateData.Data as TextMeshProUGUIColorData;
+            EditorGUI.BeginChangeCheck();
+            colorData.Color = EditorGUILayout.ColorField("Color", colorData.Color);
             if (EditorGUI.EndChangeCheck())
             {
                 EditorUtility.SetDirty(controller);
